62d8037964c3bbdcfd80728c54ea56d50c0a81cb
Walmir Alves <walmirjr915@gmail.com>
2025-04-25 20:39:35 -0300

    Create SellerDaoJDBC and DaoFactory classes

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,36 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller seller) {
+        // TODO
+    }
+
+    @Override
+    public void update(Seller seller) {
+        // TODO
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        // TODO
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        // TODO
+        return null;
+    }
+}
